173c4ce9fd3c294e0dea86e8a97dab54a03417ce
jambrose0 <jambrose0518@gmail.com>
2022-02-11 16:27:17 -0500

    call functions to weathermap are live

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,97 @@
-var searchBtn = $("#searchBtn");
+//url for lat and long
+
+var key = "480554773b84ca0bbe144db4c1fb5c42";
+var searchEl = $("#get-city");
+
+function citySearch() {
+  var cityName = searchEl.val();
+  var search = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
+  fetch(search)
+    .then(function (response) {
+      if (200 !== response.status) {
+        console.log("There's a problem dude.  Status:" + response.status);
+      }
+      return response.json();
+    })
+    .then(function (response) {
+      var place = response.coord;
+      getForecast(place.lat, place.lon);
+    });
+}
+function getForecast(lat, lon) {
+  var cityWeather = `https:api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=480554773b84ca0bbe144db4c1fb5c42`;
+  fetch(cityWeather)
+    .then(function (response) {
+      if (200 !== response.status) {
+        console.log("There's a problem.  Status:" + response.status);
+      }
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+
+      //display this
+      //check docs or console log for info that i need
+    });
+}
+//template literal approach- more modern way
 
 $("#searchBtn").click(function () {
-  alert("AWOOOO");
+  citySearch();
 });
+// function getForecast() {
+//   var weatherLink =
+("https:api.openweathermap.org/data/2.5/onecall?lat=35.582420347215304&lon=-82.55717434127367&exclude=hourly,minutely&alerts&appid=480554773b84ca0bbe144db4c1fb5c42");
+//   var forecastEl = document.getElementsByClassName("forecast");
 
-//
-Promise.all([
-  fetch(
-    "https:api.openweathermap.org/data/2.5/onecall?lat=insert LAT here&lon=insertLON here&exclude={part}&appid=480554773b84ca0bbe144db4c1fb5c42"
-  ),
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=480554773b84ca0bbe144db4c1fb5c42"
-  ),
-]);
+//   fetch(weatherLink)
+//     .then(function (response) {
+//       if (200 !== response.status) {
+//         console.log("There's a problem dude.  Status:" + response.status);
+//         return;
+//       }
+//       forecastEl[0].classList.add("loaded");
+//       response.json().then(function (data) {
+//         var fday = "";
+//         data.daily.forEach((value, index) => {
+//           if (index > 0) {
+//             var weekDay = new Date(value.dt * 1000).toLocaleDateString("en", {
+//               weekday: "long",
+//             });
+//             var icon = value.weather[0].icon;
+//             var temp = value.temp.day.toFixed(0);
+//             fday = `<div class="forecast-day"> <p>${weekDay}</p> <p><span class="ico-${icon}"title="${icon}"></span></p><div class='forecast-day--temp">${temp}<sup>°F</sup></div></div>`;
+//             forecastEl[0].insertAdjacentHTML("beforeend", fday);
+//           }
+//         });
+//       });
+//     })
+//     .catch(function (err) {
+//       console.log("Fetch erro :-S", err);
+//     });
+// }
+// document.addEventListener("DOMContentLoaded", function () {
+//   var weather;
+//   if ("InstersectionObserver" in window) {
+//     weather = document.querySelectorAll(".weather");
+//     var weatherObserver = new IntersectionObserver(
+//       function (entries, observer) {
+//         entries.forEach(function (entry) {
+//           if (entry.isIntersecting) {
+//             if (entry.target.classList.contains("weather")) {
+//               fetchForecast();
+//             }
+//           }
+//         });
+//       },
+//       {
+//         rootMargin: "0px 0px-120px 0px",
+//       }
+//     );
+//     weather.forEach(function (s) {
+//       weatherObserver.observe(s);
+//     });
+//   }
+// });
 
-fetch(
-  "https:api.openweathermap.org/data/2.5/onecall?lat=insert LAT here&lon=insertLON here&exclude={part}&appid=480554773b84ca0bbe144db4c1fb5c42"
-).then(
-  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=480554773b84ca0bbe144db4c1fb5c42"
-);
+// // var searchBtn = $("#searchBtn");
