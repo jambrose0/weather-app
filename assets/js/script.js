cfbafe741b0d9457e8f3e12589aabb97fd91de16
jambrose0 <jambrose0518@gmail.com>
2022-02-13 14:24:37 -0500

    display one day of weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,11 @@ function citySearch() {
     })
     .then(function (response) {
       var place = response.coord;
-      getForecast(place.lat, place.lon, place.temp);
+      getForecast(place.lat, place.lon, place.temp, cityName);
       // console.log(getForecast);
     });
 }
-function getForecast(lat, lon) {
-  var cities = citySearch.cityName;
+function getForecast(lat, lon, cityName) {
   var cityWeather = `https:api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=imperial&appid=480554773b84ca0bbe144db4c1fb5c42`;
   fetch(cityWeather)
     .then(function (response) {
@@ -30,19 +29,54 @@ function getForecast(lat, lon) {
       return response.json();
     })
     .then(function (response) {
-      var tempEl = response.current.temp;
-      // var windEl = response.wind_speed;  how to get wind speed?
-      var uvEl = response.current.uvi;
-      var humEl = response.current.humidity;
-      console.log(response);
       //display city and date
-      `<h3>${cities}</h3>`;
+
       //display temp
       //display wind
       //display humidity
       //display uv index
-
-      //check docs or console log for info that i need
+      //step1: create text content, i.e. h2
+      //step2: template literal var.textContent = Humidity: $(humEl);
+      //step3: append to the appropriate div
+      var cities = citySearch.cityName;
+      var codeIcon = response.current.weather[0].icon;
+      var iconEl = `http://openweathermap.org/img/w/${codeIcon}.png`;
+      // console.log(cities);
+      var tempEl = response.current.temp;
+      var windEl = response.current.wind_speed;
+      var uvEl = response.current.uvi;
+      var humEl = response.current.humidity;
+      var dateEl = response.current.dt;
+      var showDate = new Date(dateEl * 1000).toLocaleDateString("en-US");
+      console.log(showDate);
+      console.log(response);
+      console.log(codeIcon);
+      console.log("awooo");
+      // $("#city-weather").append($("<h3>").text(cityName));
+      // $("#city-weather").append($(iconEl));
+      $("#city-weather").append($("<li>").addClass("details").text(showDate));
+      $("#city-weather").append(
+        $("<li>")
+          .addClass("details")
+          .text("Temp: " + tempEl)
+      );
+      $("#city-weather").append(
+        $("<li>")
+          .addClass("details")
+          .text("Wind: " + windEl)
+      );
+      $("#city-weather").append(
+        $("<li>")
+          .addClass("details")
+          .text("Humidity: " + humEl)
+      );
+      $("#city-weather").append(
+        $("<li>")
+          .addClass("details")
+          .text("UV Index: " + uvEl)
+      );
+      //5day, date, icon, temp, wind, humidity
+      // var fiveDay =
     });
 }
 //template literal approach- more modern way
@@ -50,59 +84,3 @@ function getForecast(lat, lon) {
 $("#searchBtn").click(function () {
   citySearch();
 });
-// function getForecast() {
-//   var weatherLink =
-("https:api.openweathermap.org/data/2.5/onecall?lat=35.582420347215304&lon=-82.55717434127367&exclude=hourly,minutely&alerts&appid=480554773b84ca0bbe144db4c1fb5c42");
-//   var forecastEl = document.getElementsByClassName("forecast");
-
-//   fetch(weatherLink)
-//     .then(function (response) {
-//       if (200 !== response.status) {
-//         console.log("There's a problem dude.  Status:" + response.status);
-//         return;
-//       }
-//       forecastEl[0].classList.add("loaded");
-//       response.json().then(function (data) {
-//         var fday = "";
-//         data.daily.forEach((value, index) => {
-//           if (index > 0) {
-//             var weekDay = new Date(value.dt * 1000).toLocaleDateString("en", {
-//               weekday: "long",
-//             });
-//             var icon = value.weather[0].icon;
-//             var temp = value.temp.day.toFixed(0);
-//             fday = `<div class="forecast-day"> <p>${weekDay}</p> <p><span class="ico-${icon}"title="${icon}"></span></p><div class='forecast-day--temp">${temp}<sup>°F</sup></div></div>`;
-//             forecastEl[0].insertAdjacentHTML("beforeend", fday);
-//           }
-//         });
-//       });
-//     })
-//     .catch(function (err) {
-//       console.log("Fetch erro :-S", err);
-//     });
-// }
-// document.addEventListener("DOMContentLoaded", function () {
-//   var weather;
-//   if ("InstersectionObserver" in window) {
-//     weather = document.querySelectorAll(".weather");
-//     var weatherObserver = new IntersectionObserver(
-//       function (entries, observer) {
-//         entries.forEach(function (entry) {
-//           if (entry.isIntersecting) {
-//             if (entry.target.classList.contains("weather")) {
-//               fetchForecast();
-//             }
-//           }
-//         });
-//       },
-//       {
-//         rootMargin: "0px 0px-120px 0px",
-//       }
-//     );
-//     weather.forEach(function (s) {
-//       weatherObserver.observe(s);
-//     });
-//   }
-// });
-
-// // var searchBtn = $("#searchBtn");
